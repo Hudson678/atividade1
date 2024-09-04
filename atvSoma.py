4747a171df4607e9878536c227ae453ff4a65d65
Hudson678 <Hudsonmores31@gmail.com>
2024-09-04 14:45:59 -0300

    soma de dois numeros

---
+++ atvSoma.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+num1 = int(input("digite um numero: "))
+num2 = int(input("digite um segundo numero: "))
+
+soma = (num1 + num2)
+print (f"a soma deu o resultado {soma}!")
